b6ea3b5714fc8907816b41e205a5ed5a2804a777
David Steinberg <david@sonabuzz.com>
2022-09-05 01:57:06 +0200

    Add license header comment

--- Games/GrayscaleTest/GrayscaleTest.py
+++ Games/GrayscaleTest/GrayscaleTest.py
@@ -4,6 +4,20 @@
 # Shows six different screens, then reboots. Cycle through the screens by
 # pressing A or B.
 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 # Fix import path so it can find a local grayscale library if present
 from sys import path as syspath
 syspath.insert(0, '/Games/GrayscaleTest')
